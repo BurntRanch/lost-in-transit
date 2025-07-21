b077e0ae4c467cb521f766e000377fa4dc24c244
Toni500git <toni500minecraft@gmail.com>
2025-07-21 19:55:24 +0200

    add actual options

---
+++ include/options.h
@@ -0,0 +1,10 @@
+#ifndef _OPTIONS_H_
+#define _OPTIONS_H_
+
+#include <stdbool.h>
+
+static struct Options_t {
+    bool vsync;
+} options;
+
+#endif // !_OPTIONS_H_
--- src/scenes/options.c
+++ src/scenes/options.c
@@ -1,49 +1,126 @@
 #include "scenes/options.h"
+#include "button.h"
 #include "engine.h"
+#include "options.h"
 #include "scenes.h"
 #include "label.h"
 
 #include <SDL3/SDL_error.h>
+#include <SDL3/SDL_hints.h>
 #include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3_image/SDL_image.h>
 #include <stdio.h>
 
 static SDL_Renderer *renderer = NULL;
 
-static struct LE_Label title_label;
-static struct SDL_FRect title_dstrect = {0, 0, 0, 0};
+static struct LE_Label option_vsync_label;
+static struct LE_RenderElement option_vsync_element;
+static struct LE_Button option_vsync_button;
+static struct SDL_Texture *checkbox_option_vsync_texture;
+static struct LE_RenderElement checkbox_option_vsync_element;
+static struct LE_Button checkbox_option_vsync_button;
 
-static double time_elapsed;
+static struct SDL_Texture *back_texture;
+static struct LE_RenderElement back_element;
+static struct LE_Button back_button;
+
+static inline void BackButtonPressed() {
+    LEScheduleLoadScene(SCENE_MAINMENU);
+}
+
+const char *checkbox_image;
+
+static inline void OptionVsyncButtonPressed() {
+    options.vsync = !options.vsync;
+    checkbox_image = options.vsync ? "images/checkbox_true.png" : "images/checkbox_false.png";
+    if (!(checkbox_option_vsync_texture = IMG_LoadTexture(renderer, checkbox_image))) {
+        fprintf(stderr, "Failed to load '%s'! (SDL Error Code: %s)\n", checkbox_image, SDL_GetError());
+        return;
+    }
+    checkbox_option_vsync_element.texture = &checkbox_option_vsync_texture;
+}
 
 bool OptionsInit(SDL_Renderer *pRenderer) {
     renderer = pRenderer;
 
-    time_elapsed = 0.0;
+    // Back button
+    if (!(back_texture = IMG_LoadTexture(renderer, "images/back.png"))) {
+        fprintf(stderr, "Failed to load 'images/back.png'! (SDL Error Code: %s)\n", SDL_GetError());
+        return false;
+    }
+    back_element.texture = &back_texture;
+    back_element.dstrect.w = (*back_element.texture)->w;
+    back_element.dstrect.h = (*back_element.texture)->h;
 
-    title_label.text = "nothing here! switching back in 5 seconds";
-    if (!UpdateText(&title_label)) {
+    InitButton(&back_button);
+    back_button.max_angle = -10.0f;
+    back_button.element = &back_element;
+    back_button.on_button_pressed = BackButtonPressed;
+
+    // Vsync option
+    option_vsync_label.text = "Enable Vsync (useless)";
+    if (!UpdateText(&option_vsync_label)) {
         return false;
     }
 
-    title_dstrect.w = title_label.surface->w;
-    title_dstrect.h = title_label.surface->h;
+    option_vsync_element.texture = &option_vsync_label.texture;
+    option_vsync_element.dstrect.w = option_vsync_label.surface->w;
+    option_vsync_element.dstrect.h = option_vsync_label.surface->h;
+
+    InitButton(&option_vsync_button);
+    option_vsync_button.max_angle = -2.5f;
+    option_vsync_button.element = &option_vsync_element;
+    SDL_SetTextureColorMod(*option_vsync_button.element->texture, 100, 100, 200);
+
+    // Check box (option_vsync)
+    checkbox_image = options.vsync ? "images/checkbox_true.png" : "images/checkbox_false.png";
+    if (!(checkbox_option_vsync_texture = IMG_LoadTexture(renderer, checkbox_image))) {
+        fprintf(stderr, "Failed to load '%s'! (SDL Error Code: %s)\n", checkbox_image, SDL_GetError());
+        return false;
+    }
+    checkbox_option_vsync_element.texture = &checkbox_option_vsync_texture;
+    checkbox_option_vsync_element.dstrect.w = (*checkbox_option_vsync_element.texture)->w / 5.5f;
+    checkbox_option_vsync_element.dstrect.h = (*checkbox_option_vsync_element.texture)->h / 5.5f;
+
+    InitButton(&checkbox_option_vsync_button);
+    checkbox_option_vsync_button.max_angle = -10.0f;
+    checkbox_option_vsync_button.element = &checkbox_option_vsync_element;
+    checkbox_option_vsync_button.on_button_pressed = OptionVsyncButtonPressed;
 
     return true;
 }
 
 bool OptionsRender(void) {
-    time_elapsed += LEFrametime;
+    back_element.dstrect.x = LEScreenWidth * 0.0125f;
+    back_element.dstrect.y = LEScreenHeight * 0.0125f;
 
-    if (time_elapsed > 5) {
-        LEScheduleLoadScene(SCENE_MAINMENU);
+    option_vsync_element.dstrect.x = SDL_max((LEScreenWidth * 0.25) - (option_vsync_element.dstrect.w / 2), 0);
+    option_vsync_element.dstrect.y = SDL_max((LEScreenHeight * 0.2) - (option_vsync_element.dstrect.h / 2), 0);
+    checkbox_option_vsync_element.dstrect.x = option_vsync_element.dstrect.x * 8.0f;
+    checkbox_option_vsync_element.dstrect.y = option_vsync_element.dstrect.y * 0.95f;
+
+    struct MouseInfo mouse_info;
+    mouse_info.state = SDL_GetMouseState(&mouse_info.x, &mouse_info.y);
+
+    if (!ButtonStep(&back_button, &mouse_info, &LEFrametime)) {
+        return false;
+    }
+    if (!ButtonStep(&checkbox_option_vsync_button, &mouse_info, &LEFrametime)) {
+        return false;
     }
 
-    title_dstrect.x = LEScreenWidth * 0.0125;
-    title_dstrect.y = LEScreenHeight * 0.0125;
-
-    if (!SDL_RenderTexture(renderer, title_label.texture, NULL, &title_dstrect)) {
+    if (!SDL_RenderTextureRotated(renderer, *back_element.texture, NULL, &back_element.dstrect, back_button.angle, NULL, SDL_FLIP_NONE)) {
+        fprintf(stderr, "Failed to render back button! (SDL Error Code: %s)\n", SDL_GetError());
+        return false;
+    }
+    if (!SDL_RenderTexture(renderer, *option_vsync_button.element->texture, NULL, &option_vsync_element.dstrect)) {
         fprintf(stderr, "Failed to render options text! (SDL Error Code: %s)\n", SDL_GetError());
+        return false;
+    }
+    if (!SDL_RenderTextureRotated(renderer, *checkbox_option_vsync_element.texture, NULL, &checkbox_option_vsync_element.dstrect, checkbox_option_vsync_button.angle, NULL, SDL_FLIP_NONE)) {
+        fprintf(stderr, "Failed to render checkbox_option_vsync button! (SDL Error Code: %s)\n", SDL_GetError());
         return false;
     }
 
@@ -51,5 +128,7 @@ bool OptionsRender(void) {
 }
 
 void OptionsCleanup(void) {
-    DestroyText(&title_label);
+    SDL_DestroyTexture(back_texture);
+    SDL_DestroyTexture(checkbox_option_vsync_texture);
+    DestroyText(&option_vsync_label);
 }
